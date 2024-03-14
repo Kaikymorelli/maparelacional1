ed267aff37d30ba705a216c3e006120bbcd68ca5
Kaikymorelli <132359674+Kaikymorelli@users.noreply.github.com>
2024-03-14 14:03:23 -0300

    Add files via upload

---
+++ Projetomapad/src/main/java/com/Projetomapa/ProjetomapadApplication.java
@@ -0,0 +1,13 @@
+package com.Projetomapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomapadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomapadApplication.class, args);
+	}
+
+}
---
+++ Projetomapad/src/test/java/com/Projetomapa/ProjetomapadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projetomapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomapadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
